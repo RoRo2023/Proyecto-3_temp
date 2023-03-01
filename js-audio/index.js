7a28956d38b81f583fccd834c4bd172e13209f67
Jesus-007-cmd <jesusgtz1@hotmail.com>
2023-03-01 17:29:33 -0600

    se elimino la rama audio ya que no es nesearia

--- js-audio/index.js
+++ js-audio/index.js
@@ -1,5 +1,7 @@
+
+//El siguiente código permitira hacer comandos de voz si es que nos alcanza el tiempo, de lo contrario solo dara un mensaje de bienvenida con voz
 document.addEventListener("DOMContentLoaded", function() {
-    
+    //11111111111  En esta sección da el mensaje de bienvenida de nuestra aplicación 11111111111111111
         var voz;
         voz = window.speechSynthesis.getVoices();
         var confvoz = new SpeechSynthesisUtterance();
@@ -7,8 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
         confvoz.pitch = 0.5;
         confvoz.lang = 'es-MX';
         confvoz.text = 'Bienvenido a Nuestra aplicación de API';
-        
-
-        confvoz.voice = voz[2];
+        confvoz.voice = voz[0];
         window.speechSynthesis.speak(confvoz);
+    //11111111111Aqui finaliza la parte del mensaje de bienvenida de voz de nuestra aplicación11111111111111111
         })
